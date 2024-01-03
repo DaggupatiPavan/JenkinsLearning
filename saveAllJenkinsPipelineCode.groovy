9cf72c915aab779ae83b11e9a33e3e8d9558d99a
DaggupatiPavan <97659712+DaggupatiPavan@users.noreply.github.com>
2024-01-03 09:53:45 +0530

    Update saveAllJenkinsPipelineCode.groovy

--- saveAllJenkinsPipelineCode.groovy
+++ saveAllJenkinsPipelineCode.groovy
@@ -1,31 +1,38 @@
-// To save all pipeline code into files, run the below script in the Jenkins script console. You will also need to update the file path on line 16 to match your system.
+// To save all pipeline code into files, run the below script in the Jenkins script console. You will also need to update the file path on line 10 to match your system.
 
-import jenkins.model.Jenkins
+import jenkins.model.*
 import java.nio.file.*
 
-def savePipelineJob(job) {
-    def scriptContent
+def jenkins = Jenkins.instance
 
-    if (job.definition instanceof org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition) {
-        scriptContent = job.definition.getScriptPath()
-    } else if (job.definition instanceof org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition) {
+jenkins.views.each { view ->
+  
+  def viewPath = "/var/lib/jenkins/jenkins_files/${view.name}"
+  
+  new File(viewPath).mkdirs()
+
+  view.items.each { job ->
+
+    if (job instanceof org.jenkinsci.plugins.workflow.job.WorkflowJob) {
+
+      def scriptContent 
+
+      if (job.definition instanceof org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition) {
+        scriptContent = job.definition.getScriptPath()  
+      } else if (job.definition instanceof org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition) {
         scriptContent = job.definition.getScript()
-    }
+      }
 
-    if (scriptContent) {
-        def filePath = "/var/lib/jenkins/jenkins_files/${job.name}.groovy"
-
+      if (scriptContent) {
+        def jobPath = "${viewPath}/${job.name}.groovy"
+        
         try {
-            Files.write(Paths.get(filePath), scriptContent.getBytes())
-            println("Job '${job.name}' saved successfully at $filePath")
+          Files.write(Paths.get(jobPath), scriptContent.getBytes())
+          println "Saved job '${job.name}' to ${jobPath}"
         } catch (IOException e) {
-            println("Error saving job '${job.name}': $e.message")
+          println "Error saving '${job.name}': ${e.message}"
         }
-    } else {
-        println("Job '${job.name}' does not have a valid pipeline script.")
+      }
     }
-}
-
-Jenkins.instance.getAllItems(org.jenkinsci.plugins.workflow.job.WorkflowJob.class).each { job ->
-    savePipelineJob(job)
+  }
 }
